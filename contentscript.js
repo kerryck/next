c220c2c893a028d83b60e51b7a5c597f1d173f7d
kerryck <kerryck@lappy386.(none)>
2011-10-09 21:49:16 -0700

    Revamp of everything.

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,17 @@
-document.body.onkeydown = function(evt){
-    console.log(evt.altKey, evt.keyCode);
-    if(evt.altKey){
-        if(evt.keyCode === 187){
-            console.log("sending");
-            chrome.extension.sendRequest({changePage: true, changeAmmount: 1});
-        }else if (evt.keyCode === 189){
-             console.log("sending");
-            chrome.extension.sendRequest({changePage: true, changeAmmount: -1});
+(function(){
+    document.body.addEventListener('keydown', function(evt){
+        if(evt.ctrlKey){
+            if(evt.keyCode === 190){ // 190 is the keycode for ">"
+                chrome.extension.sendRequest({
+                    changePage: true,
+                    changeBy: 1
+                });
+            } else if (evt.keyCode === 188){ // 188 is the keyCode for "<"
+                chrome.extension.sendRequest({
+                    changePage: true,
+                    changeBy: -1
+                });
+            }
         }
-    }
-};
+    });
+})();
